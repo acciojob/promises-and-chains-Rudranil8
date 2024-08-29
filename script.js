ecf192324f2d135ded9c39cff6077907a3347b50
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 11:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+// let x=document.querySelector("#age");
+// let y=document.querySelector("#name");
+// document.querySelector("#btn").addEventListener("mouseover", ()=>{
+// 	alert(x.value);
+// })
+
+document.querySelector("#btn").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let age = document.querySelector("#age").value;
+  let name = document.querySelector("#name").value;
+
+  new Promise((resolve, reject) => {
+    if (age > 18) {
+      setTimeout(() => {
+        resolve(`Welcome, ${name}. You can vote.`);
+      }, 4000);
+    } else {
+      reject(`Oh sorry ${name}. You aren't old enough.`);
+    }
+  })
+  .then(message => {
+    alert(message);
+  })
+  .catch(message => {
+    alert(message);
+  });
+});

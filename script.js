5f79a91d329bca35de7b13aded14703be9244815
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 12:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,30 @@
 // 	alert(x.value);
 // })
 
+// document.querySelector("#btn").addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   let age = document.querySelector("#age").value;
+//   let name = document.querySelector("#name").value;
+
+//   new Promise((resolve, reject) => {
+//     if (age > 18) {
+//       setTimeout(() => {
+//         resolve(`Welcome, ${name}. You can vote.`);
+//       }, 4000);
+//     } else {
+//       reject(`Oh sorry ${name}. You aren't old enough.`);
+//     }
+//   })
+//   .then(message => {
+//     alert(message);
+//   })
+//   .catch(message => {
+//     alert(message);
+//   });
+// });
+
+
 document.querySelector("#btn").addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -11,11 +35,18 @@ document.querySelector("#btn").addEventListener("click", (event) => {
   let name = document.querySelector("#name").value;
 
   new Promise((resolve, reject) => {
-    if (age > 18) {
+    // Check if inputs are empty
+    if (age === "" || name === "") {
+      reject("Please enter valid details");
+    } 
+    // Check if age is above 18
+    else if (age > 18) {
+      // If yes, resolve the promise after 4 seconds
       setTimeout(() => {
         resolve(`Welcome, ${name}. You can vote.`);
       }, 4000);
     } else {
+      // If no, reject the promise immediately
       reject(`Oh sorry ${name}. You aren't old enough.`);
     }
   })
